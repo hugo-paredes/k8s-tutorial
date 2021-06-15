4aab901f33ffaf776f8090aaf5fb93153e564a6b
Hugo Paredes <hugo.nogueira.paredes@allianz.nl>
2021-06-15 10:23:34 +0200

    nginx implementation

--- src/cli/k8s-commands.sh
+++ src/cli/k8s-commands.sh
@@ -4,10 +4,13 @@ echo " * Delete namespace"
 kubectl delete namespace k8s-demo-namespace
 
 echo " * Create namespace"
-kubectl apply -f ../k8s-files/namespace.yaml
+kubectl apply -f ./src/k8s-files/namespace.yaml
 
-echo " * Create webserver"
-kubectl apply -f ../k8s-files/namespace.yaml
+echo " * Create deployment"
+kubectl apply -f ./src/k8s-files/deployment.yaml
+
+echo " * Create service"
+kubectl apply -f ./src/k8s-files/service.yaml
 
 echo " * Create ingress"
-kubectl apply -f ../k8s-files/ingress.yaml
+kubectl apply -f ./src/k8s-files/ingress.yaml
